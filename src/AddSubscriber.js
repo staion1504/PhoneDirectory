d6105e5daad2b0d40645bfbb877b6e7bbe846932
staion1504 <pagillasaitejauma@gmail.com>
2023-10-12 01:13:09 +0530

    used material ui

--- src/AddSubscriber.js
+++ src/AddSubscriber.js
@@ -2,7 +2,7 @@ import React, { Component, useState } from "react";
 import Header from "./Header";
 import './AddSubscriber.css';
 import { Link, useNavigate } from 'react-router-dom';
-
+import { ValidatorForm,TextValidator } from "react-material-ui-form-validator";
 
 export default function AddSub(props) {
     
@@ -35,18 +35,45 @@ export default function AddSub(props) {
             <Header heading='Add Subscriber' />
             <div className="component-body-conatiner">
                 <Link to="/"><button className="addButton">Back</button></Link>
-                <form className="subscriber-form" onSubmit={onFormSubmitted}>
-                    <label htmlFor="name" className="label-control" >Name:</label><br />
-                    <input id="name" type="text" className="input-control" name="name" onChange={changeHandler} /><br /><br />
-                    <label htmlFor="phone" className="label-control">Phone:</label><br />
-                    <input id="phone" type="text" className="input-control" name="phone" onChange={changeHandler} /><br /><br />
+                <ValidatorForm className="subscriber-form" onSubmit={onFormSubmitted}>
+                    
+                  
+
+                   <TextValidator
+                   
+                   id="name" type="text" 
+                   name="name" onChange={changeHandler}
+                   label="name"
+                   value={state.name}
+                   validators={['required']}
+                   errorMessages={['Name cannot be empty']}
+                   >
+                   </TextValidator>
+
+                    <br /><br />
+
+                    <TextValidator
+                   
+                   id="phone" type="text"
+                   name="phone" onChange={changeHandler}
+                   label="Phone"
+                   value={state.phone}
+                   validators={['required']}
+                   errorMessages={['Phone cannot be empty']}
+                   >
+                   </TextValidator>
+
+
+
+                    
+                
                     <div className="subscriber-info-container">
                         <span className="subscriber-to-add-heading">Subcriber to be added: </span><br />
                         <span className="subscriber-info">Name:{state.name} </span><br />
                         <span className="subscriber-info">Phone:{state.phone} </span>
                     </div>
                     <button type="submit" className="addButton">Add</button>
-                </form>
+                </ValidatorForm>
             </div>
         </div>
 
